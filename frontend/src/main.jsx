27a398c364f9c8673180c646ca10dcfb54352d2a
thaddichakradhar <164134180+thaddichakradhar@users.noreply.github.com>
2025-08-05 12:29:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,29 @@
+// main.jsx
+
 import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+
 import App from './App.jsx'
+
 import { CartProvider } from "./context/CartContext";
 
+import { BrowserRouter } from "react-router-dom"
+
+
+
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
-  <App />
-</CartProvider>
+
+  <BrowserRouter basename="/ecommerce">
+
+    <CartProvider>
+
+      <App />
+
+    </CartProvider>
+
+  </BrowserRouter>
+
 )
